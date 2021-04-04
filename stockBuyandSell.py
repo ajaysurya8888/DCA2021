fd8f298360cf8aaf014e74d24985e01c5d530747
Queen Coder <61905660+queencodehub@users.noreply.github.com>
2021-04-04 15:58:30 +0530

    Add files via upload
    This program finds out maximum profit that a person can  make by buying and selling stocks.

---
+++ stockBuyandSell.py
@@ -0,0 +1,16 @@
+def maxProfit(prices):
+    n=len(prices)
+    cost=0
+    maxcost=0
+    if n==0:
+        return 0
+    min_price=0
+    for i in range(n):
+        min_price=min(min_price,prices[i])
+        cost=prices[i]-min_price
+        maxcost=max(maxcost,cost)
+    return maxcost
+prices=[7,1,5,3,6,4]
+print(maxProfit(prices))
+
+
